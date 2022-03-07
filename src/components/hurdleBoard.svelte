a8e8751b4638799507cd6891cdbf349a4f8799fa
Samuel C Tyler <skamansam@gmail.com>
2022-03-06 23:55:30 -0500

    make sure we can validate inputs

--- src/components/hurdleBoard.svelte
+++ src/components/hurdleBoard.svelte
@@ -1,21 +1,89 @@
-<script>
+<script lang="ts">
+
 import WordInput from "./wordInput.svelte";
 import { range } from '../lib/helpers';
-export let numTries = 5;
-export let wordlength = 5;
-let guesses = ["helps"];
-let currentGuessIndex = 0;
+import Keyboard from "./keyboard.svelte";
 
-// TODO: there should be a single input field that holds focus. on text change, it should update the view. on return, it should calculate the answers.
+export let numTries: number = 5;
+export let wordLength: number = 5;
+let guessedWords: string[] = [""];
+let currentGuessIndex: number = 0;
+let word: string = 'guess';
+let guessedLetters = {};
+let error: String = null;
+let correctnessMap: string[][] = [];
+
+const calculateCorrectness = (): undefined => {
+  let currentWord: String = guessedWords[currentGuessIndex].toLowerCase();
+  const guessArray: string[] = currentWord.toLowerCase().split('');
+  const wordArray: string[] = word.toLowerCase().split('');
+  if (currentWord === word) {
+    guessArray.forEach((l)=> guessedLetters[l] = 'c');
+    correctnessMap[currentGuessIndex] = guessArray.map(() => 'c');
+    return;
+  }
+  correctnessMap[currentGuessIndex] = guessArray.map((l, index) => {
+    if(l === wordArray[index]) return 'c';  // "c"orrect
+    if (wordArray.includes(l)) return 'p';  // "p"resent
+    return 'm';                               // "m"issing
+  });
+  console.log(correctnessMap, word, guessArray);
+}
+
+const validateWord = (word: string) => {
+  if (word.length < wordLength) {
+      error = `Your Guess must be ${wordLength} letters.`;
+      return false;
+    }
+    return true;
+}
+
+const checkWord = (word: string) => {
+  if (!validateWord(word)) return;
+  calculateCorrectness();
+  currentGuessIndex = currentGuessIndex + 1;
+  guessedWords[currentGuessIndex] = '';
+}
+
+const deleteLastLetter = (word: string) => {
+  if (word.length < 1 ) return;
+  guessedWords[currentGuessIndex] = word.slice(0,-1);
+  guessedWords = [...guessedWords];
+}
+
+const appendLetter = (word: string, key: string) => {
+  if (word.length >= wordLength || key.length !== 1 || key > 'Z' || key < 'A' ) return;
+  guessedWords[currentGuessIndex] = `${word}${key}`;
+  guessedWords = [...guessedWords];
+
+}
+
+const handleKeydown = ({key}) => {
+  error = null;
+  key = key.toUpperCase();
+  let currentWord = guessedWords[currentGuessIndex];
+  if (key === 'ENTER') checkWord(currentWord);
+  if (key === 'BACKSPACE') deleteLastLetter(currentWord);
+  appendLetter(currentWord, key);
+}
+const handleStateChanged = (index: Number, evt: Event) => {
+  console.log("Handling state change for ", index, evt)
+
+}
 </script>
-
-<div class="flex justify-center items-center my-4">
+<svelte:window on:keydown={handleKeydown}/>
+<div class="flex justify-center items-center my-4 flex-col">
+  {#if error}
+  <div class="mb-5 rounded border w-full text-center p-2 bg-red-400 text-slate-900  border-slate-900">{error}</div>
+  {/if}
   <ul>
-    {#each range(0, numTries) as name, index (name)}
-      <li class="pb-4">
-        <WordInput wordlength="{wordlength}" word="{guesses[index]}"></WordInput>
+    {#each range(0, numTries) as index (index)}
+      <li class="pb-4 {index}">
+        <WordInput wordlength="{wordLength}" word="{guessedWords[index] || ''}" validations={correctnessMap[index]} on:stateChanged="{(evt) =>  handleStateChanged(index, evt)}"></WordInput>
       </li>
     {/each}
-    <input bind:value="{guesses[currentGuessIndex]}">
   </ul>
-</div>
+  <div class="flex justify-center items-center my-4">
+    <Keyboard guessedLetters={correctnessMap}></Keyboard>
+  </div></div>
+
