ca06eec1db49de8685e453fc8a09b2d7d6c50c5d
Samuel C Tyler <skamansam@gmail.com>
2022-03-06 23:56:10 -0500

    make sure the potential list is displayed correctly

--- src/components/potentialsList.svelte
+++ src/components/potentialsList.svelte
@@ -1,17 +1,26 @@
-<script>
+<script lang="ts">
   import { range } from '../lib/helpers';
   // import { createEventDispatcher } from 'svelte';
   // const dispatch = createEventDispatcher();
   let clazz;
 	export { clazz as class };
-  export let wordlength;
+  export let wordlength; 
+  let possibleWords = []
+  const loadFile = async () => {
+    const response = await fetch(`/by_length/${wordlength}.txt`);
+    if(response.ok) {
+      possibleWords = (await response.text()).split("\n")
+      console.debug(possibleWords);
+    }
+  }
+  loadFile();
 </script>
 
-<div class="potentials-list {clazz || ''}">
-  <h3>Potential Matches</h3>
-  <ul>
-    {#each range(0, 20) as name, index (name)}
-    <li>Item {index}</li>
+<div class="potentials-list {clazz || ''} w-72">
+  <h3 class="underline">Potential Matches</h3>
+  <ul class=" overflow-auto overscroll-contains h-96 h-full">
+    {#each possibleWords as name, index (name)}
+    <li>{name}</li>
     {/each}
   </ul>
 </div>
