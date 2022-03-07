bd778bad3a5f0f9b3c2557b60318f7bbc8cad6a6
Samuel C Tyler <skamansam@gmail.com>
2022-03-06 23:57:45 -0500

    add ts types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="@sveltejs/kit" />
+
+interface stringStringType {
+  [key: string]: string;
+}
+
+declare namespace svelte.JSX {
+  interface stringStringType {
+    [key: string]: string;
+  }
+}
